bda82a7b2cee0e3f6ae82455c425530a1b1e26ad
origin <viktoryaa2000@mail.ru>
2025-07-15 23:51:24 +0300

    Corrected

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,18 +2,15 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-
-import com.pengrad.telegrambot.TelegramBot;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.scheduler.NotificationScheduler;
 import pro.sky.telegrambot.sender.NotificationSender;
 
 
-import javax.annotation.PostConstruct;
+
 import java.util.List;
 
 
@@ -25,37 +22,29 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
 
     private final NotificationSender notificationSender;
+    private final NotificationScheduler notificationScheduler;
+
 
     @Autowired
-    public TelegramBotUpdatesListener(NotificationSender notificationSender){
+    public TelegramBotUpdatesListener(NotificationSender notificationSender, NotificationScheduler notificationScheduler){
         this.notificationSender = notificationSender;
-    }
-    private TelegramLongPollingBot bot;
-
-    public TelegramBotUpdatesListener(NotificationSender notificationSender, TelegramLongPollingBot bot){
-        this.notificationSender = notificationSender;
-        this.bot = bot;
-    }
-
-    @Autowired
-    private TelegramBot telegramBot;
-
-    @PostConstruct
-    public void init() {
-        telegramBot.setUpdatesListener((UpdatesListener) this);
+        this.notificationScheduler = notificationScheduler;
     }
 
 
     @Override
     public int process(List<Update> updates) {
         for (Update update : updates) {
-            if (update.message()!=null && update.message().text()!=null){
+            if (update.message()!=null && update.message().text()!=null) {
                 String messageText = update.message().text();
-                if ("/start".equals(messageText)){
-                    long chatId = update.message().chat().id();
-                    String welcomeMessage = " Привет! Рад видеть тебя тут. Как я могу помочь? ";
+                long chatId = 0;
+                if ("/start".equals(messageText)) {
+                    chatId = update.message().chat().id();
+                    String welcomeMessage = " Привет! Чтобы установить напоминание, отправьте сообщение вида: ДД.ММ.ГГГГ ЧЧ:ММ <Сделать домашнюю работу>";
+                    notificationSender.notificationSender(chatId, welcomeMessage);
+                } else {
+                    NotificationScheduler.scheduleReminder(chatId, messageText);
 
-                    notificationSender.notificationSender(chatId,welcomeMessage);
 
                 }
             }
--- src/main/java/pro/sky/telegrambot/scheduler/NotificationScheduler.java
+++ src/main/java/pro/sky/telegrambot/scheduler/NotificationScheduler.java
@@ -4,26 +4,37 @@ package pro.sky.telegrambot.scheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 import pro.sky.telegrambot.configuration.TelegramBotConfiguration;
 import pro.sky.telegrambot.model.NotificationTask;
 import pro.sky.telegrambot.repository.NotificationTaskRepository;
 import pro.sky.telegrambot.sender.NotificationSender;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @EnableScheduling
 public class NotificationScheduler {
     private final NotificationTaskRepository notificationTaskRepository;
-    private final NotificationSender notificationSender;
+    private static  NotificationSender notificationSender;
+    private static ThreadPoolTaskScheduler taskScheduler;
+
+    private final Map<Long,String>reminders = new HashMap<>();
 
     @Autowired
     public NotificationScheduler(NotificationTaskRepository notificationTaskRepository, TelegramBotConfiguration telegramBotConfiguration){
         this.notificationTaskRepository = notificationTaskRepository;
         notificationSender = new NotificationSender(telegramBotConfiguration.getTelegramBot());
+        this.taskScheduler = new ThreadPoolTaskScheduler();
+        this.taskScheduler.initialize();
     }
 
     @Scheduled(cron = "0 0/1 * * * *")
@@ -36,5 +47,31 @@ public class NotificationScheduler {
         }
 
         }
+
+
+        public static void scheduleReminder(long chatId, String message) {
+        String[] parts = message.split("",3);
+        if (parts.length < 3){
+            notificationSender.notificationSender(chatId,"Неверный формат. Пожалуйста используйте: ДД.ММ.ГГГГ. ЧЧ:ММ <напоминание>");
+            }
+        String dateTimeStr = parts[0] + " " + parts[1];
+        String reminderText = parts[2];
+
+        LocalDateTime reminderTime = null;
+        try{
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH.mm");
+            reminderTime = LocalDateTime.parse(dateTimeStr, formatter);
+            if (reminderTime.isBefore(LocalDateTime.now()))
+            {notificationSender.notificationSender(chatId, " Время для напоминания уже прошло ");
+            }
+        }catch (Exception e){
+            notificationSender.notificationSender(chatId, " Неверный формат. Используйте: ДД.ММ.ГГГГ ЧЧ:ММ");
+        }
+        notificationSender.notificationSender(chatId, " Напоминание установлено на " + reminderTime.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")) );
+        taskScheduler.schedule(()->sendReminder(chatId,reminderText), Instant.from(reminderTime));
+    }
+    private static void  sendReminder(long chatId, String reminderText){
+        notificationSender.notificationSender(chatId, " Напоминание: " + reminderText);
+    }
     }
 
